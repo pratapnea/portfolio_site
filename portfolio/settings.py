5be5574142d0fcc47a4e17344c7ff2c137cbc4e3
pratapnea <sereversion@gmail.com>
2021-06-27 19:34:04 +0545

    os config

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nyiho%mjm_!1wr-merd%it-5r56%u3fdn)hr%lq-@)8swv8d2f'
+import os
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
